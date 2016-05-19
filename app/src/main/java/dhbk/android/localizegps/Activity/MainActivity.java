853880606ba1bb0a61945254e49303ae39ae33b7
Phong <p7157568@gmail.com>
2016-05-19 19:32:13 +0700

    add 2 fragment template

--- app/src/main/java/dhbk/android/localizegps/Activity/MainActivity.java
+++ app/src/main/java/dhbk/android/localizegps/Activity/MainActivity.java
@@ -1,7 +1,9 @@
-package dhbk.android.localizegps;
+package dhbk.android.localizegps.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import dhbk.android.localizegps.R;
 
 public class MainActivity extends AppCompatActivity {
 
